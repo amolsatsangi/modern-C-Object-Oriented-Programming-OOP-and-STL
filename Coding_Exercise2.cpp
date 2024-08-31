a9b272d9aa7078426cf79d459f079cf4a42bb513
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2024-08-31 22:13:12 +0530

    Create Coding_Exercise2.cpp

---
+++ Coding_Exercise2.cpp
@@ -0,0 +1,34 @@
+/**************************************
+
+Using cout and the insertion operator
+In this exercise, you will write a statement using cout  and the insertion operator <<  to display how many dogs Sally has.  
+
+The variable x  represents the number of dogs. 
+
+x  has already been declared and initialized for you.
+
+The output should read "Sally has 3 dogs.". 
+
+For this exercise, there is NO need to use the '\n '  character or endl  to create a new line after the output statement.
+
+Note: only a single cout statement is required for this exercise. No other code should be necessary.
+
+You can find my solution by clicking on the solution.txt file on the left pane. But please make sure you give it a go yourself first, and only check the solution if you really get stuck.
+
+
+**********************************************/
+
+#include <iostream>
+using namespace std;
+
+void sallys_dogs() {
+    
+    int x {3};
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR CODE BELOW THIS LINE---- 
+    
+    cout <<"Sally has "<<x <<" dogs.";
+    
+    //---- WRITE YOUR CODE ABOVE THIS LINE----
+}
